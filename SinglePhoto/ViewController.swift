c6204a730a4bade8ff17ad79901072782cabd5a4
virackchhom <virackchhom@icloud.com>
2021-11-16 20:27:10 -0500

    UPDATE: testing out commit from Xcode
    - added one line change
    - added another line change for multilane changes

--- SinglePhoto/ViewController.swift
+++ SinglePhoto/ViewController.swift
@@ -8,6 +8,11 @@
 import UIKit
 
 //adding comment to test out changes
+//adding another commmit to test more changes
+
+//adding multiline changes to test multilane commits and differences
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
